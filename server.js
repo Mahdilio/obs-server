4e645e2398892feb34b6cd731278dce650434f37
Mahdilio <162349259+Mahdilio@users.noreply.github.com>
2025-06-11 22:45:33 +0330

    Add files via upload

---
+++ server.js
@@ -0,0 +1,6 @@
+git init
+git add .
+git commit -m "اولین نسخه از سرور OBS"
+git branch -M main
+git remote add origin https://github.com/YOUR_GITHUB_USERNAME/obs-server.git
+git push -u origin main
